e5ca82a629866983d5b9af6a67c3eb4f55c83070
Martin Desktop <mail@martinn.de>
2022-11-06 21:27:30 +0100

    für pip vorbereitet

---
+++ main.py
@@ -0,0 +1,3 @@
+import ipv64
+
+ipv64.ipv64(10,"jumbo.vpn64.de", "K37Nh9GXfWMicVzBnaypwUsR","https://discord.com/api/webhooks/1038852319565062154/b-VHTX3bVYnz-gD_czWtIEiBklbpZ9T1FkX7Z36IYQ0ZjqvMQYJ52nXvda6U2B53JQk8")
