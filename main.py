acfdb8f123dc33552afbe33a888fbabc438cb4db
Martin Desktop <mail@martinn.de>
2022-11-06 21:35:27 +0100

    added license

--- main.py
+++
@@ -1,8 +0,0 @@
-import ipv64
-
-update_interval = 30 # seconds
-domain = "XXX.vpn64.de"
-update_key = "XXX"
-discord_webhook = "XXX"
-
-ipv64.ipv64(update_interval, domain, update_key, discord_webhook)
