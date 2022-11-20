093b86bdee8f97138df137b1814fc53c8b9b231b
Martin Desktop <mail@martinn.de>
2022-11-20 14:55:23 +0100

    update gitignore

--- setup.py
+++ setup.py
@@ -5,19 +5,20 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(
-  name = 'ipv64',         # How you named your package folder (MyLib)
-  version = '0.2.1',      # Start with a small number and increase it with every change you make
-  description = 'Updater for ipv64.net',   # Give a short description about your library
-  author = 'R60',                   # Type in your name
-  author_email = 'pypi.nmvk0@getrekt.win',      # Type in your E-Mail
+  name = 'ipv64',
+  version = '0.3.0',
+  description = 'Updater for ipv64.net',
+  author = 'R60',
+  author_email = 'pypi.nmvk0@getrekt.win',
+  url = 'https://github.com/syncip/ipv64',
   py_modules=["ipv64"],
   package_dir={'': 'src'},
-  keywords = ['ipv64', 'dyndns', 'updater'],   # Keywords that define your package best
+  keywords = ['ipv64', 'dyndns', 'updater'],
   long_description=long_description,
   long_description_content_type='text/markdown',
-  install_requires=[            # I get to this in a second
-          'dnspython',
-          'requests',
-          'argparse',
+  install_requires=[
+          'dnspython==2.2.1',
+          'requests==2.28.1',
+          'argparse==1.4.0',
       ],
 )
