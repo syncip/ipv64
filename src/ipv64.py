31311bd4419be1b1bb6faa110f5747f2aca5ef0f
Martin Lenovo <mail@wagner-martin.com>
2023-05-15 19:13:33 +0200

    nslookup and updater added

--- src/ipv64.py
+++ src/ipv64.py
@@ -3,18 +3,27 @@ from datetime import datetime as dt
 import socket
 import logging
 import json
+import time
 
-# API KEY
-api_key = "iw6OhcD2V1paymdjqvX3BrNs07eFfTC4"
+# =========================================
+# SETTINGS
+# =========================================
+APIKEY = "KEY"
+DOMAINNAME = "DOMAIN"
+DNSTYPE = "AAAA"
 
+# =========================================
 # version
-version = "0.4.0"
+# =========================================
+version = "0.5.0"
 
 # request User-Agent
-global headers
+global headers, timeout
 headers = {
     'User-Agent': 'ipv64-updater/' + str(version),
 }
+timeout = 10
+
 
 # logging
 #loglevel = logging.DEBUG
@@ -24,83 +33,144 @@ logging.basicConfig(filename='ipv64.log', encoding='utf-8', level=loglevel, form
 # =========================================
 # get the current IP of the host
 # =========================================
-def get_ip():
+def get_ip(type):
     ipv4, ipv6 = False, False
     
     # Websites to check for current ip
     websites4 = ["https://ipv4.ipapi.de", "https://ip4.anysrc.net/plain", "https://ipv4.icanhazip.com/"]
     websites6 = ["https://ipv6.ipapi.de", "https://ip6.anysrc.net/plain", "https://ipv6.icanhazip.com/"]
 
-    # timeout in seconds
-    timeout = 10
-
     # Get the current IPv4
-    for website in websites4:
-        try:
-            request4 = requests.get(website, headers=headers, timeout=timeout)
-            # if website statuscode = 200 (ok) then break the loop and take the given ip
-            if request4.status_code == 200:
-                ipv4 = request4.text.replace(" ", "").replace("\n", "")
-                logging.info(f"IPv4 from {website}: {ipv4}")
-                break
-        except:
-            ipv4 = False
+    if type == "A":
+        for website in websites4:
+            try:
+                request4 = requests.get(website, headers=headers, timeout=timeout)
+                # if website statuscode = 200 (ok) then break the loop and take the given ip
+                if request4.status_code == 200:
+                    ip = request4.text.replace(" ", "").replace("\n", "")
+                    logging.info(f"IPv4 from {website}: {ipv4}")
+                    break
+            except:
+                ip = False
 
     # Get the current IPv6
-    for website in websites6:
-        try:
-            request6 = requests.get(website, headers=headers, timeout=timeout)
-            # if website statuscode = 200 (ok) then break the loop and take the given ip
-            if request6.status_code == 200:
-                ipv6 = request6.text.replace(" ", "").replace("\n", "")
-                logging.info(f"IPv4 from {website}: {ipv6}")
-                break
-        except:
-            ipv6 = False
+    elif type == "AAAA":
+        for website in websites6:
+            try:
+                request6 = requests.get(website, headers=headers, timeout=timeout)
+                # if website statuscode = 200 (ok) then break the loop and take the given ip
+                if request6.status_code == 200:
+                    ip = request6.text.replace(" ", "").replace("\n", "")
+                    logging.info(f"IPv6 from {website}: {ipv6}")
+                    break
+            except:
+                ip = False
+    else:
+        logging.warning("error while machine ip lookup: exit")
+        exit()
 
-    return ipv4, ipv6
+
+    return ip
 
 # =========================================
 # Get the current ip of the domain
 # =========================================
-def get_domain_details(api_key, domain_check=True):
-    api_website = f"https://ipv64.net/api.php?apikey={api_key}&get_domains"
-    result = []
+def get_domain_details(domain, type):
 
-    request = requests.get(api_website)
-    status = request.status_code
-    json_data = request.json()
+    limit = 3
+    i = 0
+    ip = False
 
-    # log if website is not reachable
-    if status != 200:
-        logging.WARNING("ipv64 API not reachable")
+    # retry x time, until exit
+    while ip == False and i <= limit:
+        # ipapi.de to get nameserver data as json data
+        api_website = f"https://ipapi.de/nslookup.php?domain={domain}&type={type}&version={version}"
+
+        request = requests.get(api_website, headers=headers, timeout=timeout)
+        status = request.status_code
+        
+        # log if website is not reachable
+        if status != 200:
+            logging.warning(f"nslookup error: statuscode error {status} exit")
+            exit()
+
+        json_data = request.json()
+
+        # if response is empty
+        if json_data == []:
+            i += 1
+            logging.warning(f"nslookup type {type}: error while nslookup - no data found for domain {domain}")
+            time.sleep(3)
+            break
+        else:
+            try:
+                if type == "A":
+                    ip = json_data[0]['ip']
+                elif type == "AAAA":
+                    ip = json_data[0]['ipv6']
+                else:
+                    ip = False
+            except:
+                ip = False
+                logging.warning(f"nslookup type {type}: error while nslookup exit")
+                exit()
+            
+        if ip != False:
+            logging.info(f"nslookup type {type}: {domain}: {ip}")
+
+    return ip
+
+# =========================================
+# UPDATER
+# =========================================
+def update_ipv64(machine_ip, domain_ip, domain, apikey, type):
+
+    # check if machine has a valid ip for the dns type
+    if machine_ip == False:
+        logging.warning(f"updater: machine has no ip: please choose other DNS type")
         exit()
 
-    # get all the doamains of the account
-    if json_data["info"] == "success":
-        domains = json_data["subdomains"].keys()
-        # check all given domains
-        for domain in domains:
-            # check if domain is know
-            if domain in domain_check:
-                # get all records for found domain
-                records = json_data["subdomains"][domain]["records"]
+    # check if the right key is given
+    if len(apikey) > 24:
+        logging.warning(f"updater: wrong key, use the account update token")
+        exit()
 
-                # check all records for the found domain
-                for record in records:
-                    logging.info(f"record: {record}")
-                    
-                    record_id = record["record_id"]
-                    record_type = record["type"]
-                    record_content = record["content"]
-                    record_praefix = record["praefix"]
+    # check if type is A or AAAA
+    if type == "A":
+        ipv64_updater = f"https://ipv64.net/update.php?key={apikey}&domain={domain}&ip={machine_ip}"
+    elif type == "AAAA":
+        ipv64_updater = f"https://ipv64.net/update.php?key={apikey}&domain={domain}&ip6={machine_ip}"
 
-                    # add dict to list for working as json
-                    record_data = {"record_id": str(record_id), "domain": str(domain), "type": str(record_type), "content": str(record_content), "praefix": str(record_praefix)}
-                    result.append(record_data)
-    
-    return result
-       
+    # check if ips are not the same
+    if machine_ip == domain_ip:
+        logging.info(f"updater: no update required")
+        status = None
+    else:    
+        request = requests.get(ipv64_updater, headers=headers, timeout=timeout)
+        status = request.status_code
 
-#current_ipv4, current_ipv6 = get_ip()
-ips = json.dumps(get_domain_details(api_key, ["cloudflare.ipv64.net"]))
+        if status != 200:
+            logging.warning(f"updater: statuscode error {status}")
+            status = False
+            exit()
+
+        if status == 200:
+            logging.warning(f"updater: {status} update successfull")
+            status = True
+
+# =========================================
+# MAIN PART
+# =========================================
+
+# current machine ip
+machine_ip = get_ip(DNSTYPE)
+print(machine_ip)
+
+# current domain ip
+domain_ip = get_domain_details(DOMAINNAME, DNSTYPE)
+print(domain_ip)
+
+# update
+update = update_ipv64(machine_ip, domain_ip, DOMAINNAME, APIKEY, DNSTYPE)
+
+print(update)
